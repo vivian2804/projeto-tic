8e34123a17597bc75cae982207c5dadc090a6432
Caio <caiotaveiradecastro@gmail.com>
2023-05-25 22:29:28 -0300

    Fisrt Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+app.get('/hello', () => {
+	return 'Hello World'
+})
+
+app.listen({
+    port: 3333,
+})
+
+.then( () => {
+    console.log('Http Server running')
+})
